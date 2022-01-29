ef331f9f72aeccddf36bf803a73d0b0f10482ff6
hutaiyi <313275847@qq.com>
2022-01-29 12:23:35 +0800

    添加jwt和gin的中间件认证

--- routes.go
+++ routes.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"hutaiyi.study/gin_vue/middleware"
+
 	"github.com/gin-gonic/gin"
 	"hutaiyi.study/gin_vue/controller"
 )
@@ -8,5 +10,6 @@ import (
 func CollectRoute(r *gin.Engine) *gin.Engine {
 	r.POST("/api/auth/register", controller.Register)
 	r.POST("/api/auth/login", controller.Login)
+	r.GET("/api/auth/info", middleware.AuthMiddleware(), controller.Info)
 	return r
 }
